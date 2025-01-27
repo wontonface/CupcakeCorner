1d20a2655cf074bbfa862020a900f0ea79a55f30
Nicole Garcia <nicole@nicoleiliana.com>
2025-01-27 14:21:30 -0800

    first commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Nicole on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CupcakeCornerTests/CupcakeCornerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerTests.swift
+//  CupcakeCornerTests
+//
+//  Created by Nicole on 1/27/25.
+//
+
+import Testing
+@testable import CupcakeCorner
+
+struct CupcakeCornerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
